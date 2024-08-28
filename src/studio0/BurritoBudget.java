3f5195aaed09c1b954752c960d7b0d7df5f9e96f
zachduckworth <zachduckworth@localhost>
2024-08-28 16:37:14 -0500

    4 to 4.0 and *->/

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -9,7 +9,9 @@ public class BurritoBudget {
 		int burritosEatenWeek4 = 1;
 		
 		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
-		double averageWeeklyBurritos = totalBurritos / 4;
+		double averageWeeklyBurritos = totalBurritos / 4.0;
+		System.out.println(averageWeeklyBurritos);
+
 		
 		double burritoPrice = 7.50;
 		double averageWeeklyPrice = averageWeeklyBurritos * burritoPrice;
